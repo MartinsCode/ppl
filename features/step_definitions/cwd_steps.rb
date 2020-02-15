cd9954bf46a92c668aa835c1971fa237d0734376
Martin Brunzel <martin.brunzel@gmx.de>
2020-02-15 14:28:32 +0100

    put local config in step_definitions for not messing up prod env

--- features/step_definitions/cwd_steps.rb
+++ features/step_definitions/cwd_steps.rb
@@ -4,6 +4,10 @@ require "tmpdir"
 Given /^I am in an empty directory$/ do
   @directory = Dir.mktmpdir
   Dir.chdir @directory
+  configfile = File.open('.pplconfig', 'w')
+  configfile << '[address book]'
+  configfile << 'path="./"'
+  configfile.close
   at_exit do
     FileUtils.rm_rf @directory
   end
@@ -12,6 +16,10 @@ end
 Given /^I am in an empty address book$/ do
   @directory = Dir.mktmpdir
   Dir.chdir @directory
+  configfile = File.open('.pplconfig', 'w')
+  configfile << '[address book]'
+  configfile << 'path="./"'
+  configfile.close
   ppl "init"
   at_exit do
     FileUtils.rm_rf @directory
@@ -21,6 +29,10 @@ end
 Given /^I am in an address book with a blank contact called bob$/ do
   @directory = Dir.mktmpdir
   Dir.chdir @directory
+  configfile = File.open('.pplconfig', 'w')
+  configfile << '[address book]'
+  configfile << 'path="./"'
+  configfile.close
   ppl "init"
   ppl "add bob Bob"
   at_exit do
