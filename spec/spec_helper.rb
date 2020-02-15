22f63a0f2c75f4d0cfc84ee6e9dba8bc528870be
Martin Brunzel <martin.brunzel@gmx.de>
2020-02-15 06:23:54 +0100

    Add Simplecov for some more metrics

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,19 @@
+require 'simplecov'
 require "fakefs/safe"
 require "pathname"
 
+SimpleCov.start
+
 bin_file = Pathname.new(__FILE__).realpath
 $:.unshift File.expand_path("../../lib", bin_file)
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = [:should, :expect]
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = [:should, :receive]
+  end
+end
 
 require "ppl"
